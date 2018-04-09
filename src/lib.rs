176dfcd02d237ba936c3e79451278d1db081d129
Otávio Pace <otaviopp8@gmail.com>
2018-04-09 01:01:52 -0300

    fn: add pixel_to_complex

--- src/lib.rs
+++ src/lib.rs
@@ -68,3 +68,35 @@ fn test_parse_complex() {
     assert_eq!(parse_complex("1.25,-0.0625"), Some(Complex { re: 1.25, im: -0.0625 }));
     assert_eq!(parse_complex(",-0.0625"), None);
 }
+
+/// Given the row and column of a pixel in the output image, return the
+/// corresponding point on the complex plane.
+///
+/// `bounds` is a pair giving the width and height of the image in pixels.
+/// `pixel` is a (column, row) pair indicating a particular pixel in that image.
+/// The `upper_left` and `upper_right` parameters are points on the complex
+/// plane designating the area our image covers.
+fn pixel_to_point(bounds: (usize, usize),
+                  pixel: (usize, usize),
+                  upper_left: Complex<f64>,
+                  lower_right: Complex<f64>)
+    -> Complex<f64>
+{
+    let (width, height) = (lower_right.re - upper_left.re,
+                           upper_left.im - lower_right.im);
+
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width  / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
+        // Why subtraction here? pixel.1 increases as we go down,
+        // but the imaginary component increases as we go up.
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 100), (25, 75),
+                              Complex { re: -1.0, im:  1.0 },
+                              Complex { re:  1.0, im: -1.0 }),
+               Complex { re: -0.5, im: -0.5 });
+}
