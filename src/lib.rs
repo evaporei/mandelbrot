9684851b439b12bf7dda90e5507891a4fa8ccb84
Otávio Pace <otaviopp8@gmail.com>
2018-04-09 01:13:28 -0300

    fn: add write_image with new image dependency

--- src/lib.rs
+++ src/lib.rs
@@ -126,3 +126,24 @@ fn render(pixels: &mut [u8],
         }
     }
 }
+
+extern crate image;
+
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+
+/// Write the buffer `pixels`, whose dimensions are given by `bounds`, to the
+/// file named `filename`.
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
+    -> Result<(), std::io::Error>
+{
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels,
+                   bounds.0 as u32, bounds.1 as u32,
+                   ColorType::Gray(8))?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate mandelbrot;
+
 fn main() {
     println!("Hello, world!");
 }
